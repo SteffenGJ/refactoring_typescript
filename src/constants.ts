2743a15a7fff0e8750d25aacf51df0de1b809164
Steffen Georgi <steffen.jensen@hafnet.dk>
2024-02-27 13:53:24 +0100

    remove constants

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const TILE_SIZE = 30;
-export const FPS = 30;
-export const SLEEP = 1000 / FPS;
-export const LEFT_KEY = "ArrowLeft";
-export const UP_KEY = "ArrowUp";
-export const RIGHT_KEY = "ArrowRight";
-export const DOWN_KEY = "ArrowDown";
